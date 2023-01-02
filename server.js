651ac280eed9b6a8b23ce0a6dee7abb35b41920c
Tejas <tejasgiri910@gmail.com>
2023-01-02 19:49:56 +0530

    updated server.js

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", req.headers.origin);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
 
 app.use(
   cors({
